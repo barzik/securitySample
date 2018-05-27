3da586b9d34342f5c59a3899f082776cf56d45e8
Ran Bar-Zik <ec2-user@ip-172-31-40-222.ec2.internal>
2018-05-27 14:53:10 +0000

    initial

--- routes/examples.js
+++ routes/examples.js
@@ -9,8 +9,20 @@ router.get('/csrf-attack', function(req, res, next) {
     res.render('csrf-attack');
 });
 
+router.get('/xss', function(req, res, next) {
+    res.render('xss', { allGETParams: req.query, allPOSTParams: req.params });
+});
+
 router.get('/show-all-params', function(req, res, next) {
     res.render('show-all-params', { allGETParams: req.query, allPOSTParams: req.params });
+});
+
+router.get('/cookies', function(req, res, next) {
+    res.render('cookies');
+});
+
+router.get('/httpcookie', function(req, res, next) {
+    res.render('httpcookie');
 });
 
 
