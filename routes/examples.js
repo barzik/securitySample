338b5665285b2326bfe1ae2a3be2286d6b4a042f
Ran Bar-Zik <ec2-user@ip-172-31-40-222.ec2.internal>
2018-05-28 15:28:23 +0000

    initial

--- routes/examples.js
+++ routes/examples.js
@@ -18,13 +18,33 @@ router.get('/show-all-params', function(req, res, next) {
 });
 
 router.get('/cookies', function(req, res, next) {
+    res.cookie('HTTPOnly','some value', { maxAge: 900000, httpOnly: true });
+    res.cookie('regularCookie','some value', { maxAge: 900000, httpOnly: false });
     res.render('cookies');
 });
 
-router.get('/httpcookie', function(req, res, next) {
-    res.render('httpcookie');
+router.get('/csp', function(req, res, next) {
+    res.render('csp', { allGETParams: req.query, allPOSTParams: req.params });
 });
 
+router.get('/csp1', function(req, res, next) {
+    res.render('csp1', { allGETParams: req.query, allPOSTParams: req.params });
+});
 
+router.get('/csp2', function(req, res, next) {
+    res.render('csp2', { allGETParams: req.query, allPOSTParams: req.params });
+});
+
+router.get('/clickjacking', function(req, res, next) {
+    res.render('clickjacking', { allGETParams: req.query, allPOSTParams: req.params });
+});
+
+router.get('/clickjackingd', function(req, res, next) {
+    //res.set('Content-Security-Policy', 'frame-ancestors \'none\'');
+    //res.set('X-Frame-Options', 'DENY');
+    // DONT FORGET THE SHIFT
+    
+    res.render('clickjackingd', { allGETParams: req.query, allPOSTParams: req.params });
+});
 
 module.exports = router;
