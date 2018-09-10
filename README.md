a640964b89ebe271386cd5fdda5f91678c3b66b3
Ran Bar-Zik <ran@bar-zik.com>
2018-09-10 12:30:10 +0000

    adding presentation

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 ## This repo contains:
 1. The presentations
 2. Demos based on express\node 
-3. Please do NOT use the demo on production sites.
+3. Please do NOT use the demo on production sites or in any site that is available to public access.
--- routes/shellinjection.js
+++ routes/shellinjection.js
@@ -2,6 +2,7 @@
 
 const express = require('express');
 const router = express.Router();
+const child_process = require('child_process')
 
 router.get('/ssjs', function(req, res, next) {
     res.render('examples/shellinjection/ssjs');
@@ -18,5 +19,25 @@ router.post('/ssjs-api', function(req, res, next) {
     const futureAge = 2050 - year;
     res.json(futureAge);
 });
+
+router.get('/command-injection', function(req, res, next) {
+    res.render('examples/shellinjection/command-injection');
+});
+
+router.get('/command-injection-attack', function(req, res, next) {
+    res.render('examples/shellinjection/command-injection-attack');
+});
+
+router.post('/command-injection-api', function(req, res, next) {
+    child_process.exec(
+        'tar cvzf ./public/staticdemofiles/demo.tar ./public/staticdemofiles/' + req.body.file_path,
+        function(err, data) {
+            if (!err) {
+                res.download('./public/staticdemofiles/demo.tar');
+            }
+        });
+});
+
+
 
 module.exports = router;
