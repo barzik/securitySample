78f32092035649e743a0d671e1573552fa4ae0a2
Ran Bar-Zik <ran@bar-zik.com>
2019-05-29 14:50:09 +0000

    SAVEPOINT

--- routes/privacy.js
+++ routes/privacy.js
@@ -8,9 +8,6 @@ router.get('/', function(req, res, next) {
     res.render('index', { title: 'Express' });
 });
 
-router.get('/social', function(req, res, next) {
-    res.render('examples/privacy/social');
-});
 
 router.get('/etag', function(req, res, next) {
 
